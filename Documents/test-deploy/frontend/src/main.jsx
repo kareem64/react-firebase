41d4b84004c9a73e22798122b907b7117c34fa6d
kareem <kareemlero@gmail.com>
2025-01-18 17:54:38 +0200

    nineteen

--- Documents/test-deploy/frontend/src/main.jsx
+++ Documents/test-deploy/frontend/src/main.jsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -9,7 +10,8 @@ import {AuthContextProvider}from './context/AuthContextProvider.jsx'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <StrictMode>
+     <BrowserRouter>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <ToastContainer position='top-center' theme='colored'/>
    <AuthContextProvider>
@@ -22,4 +24,6 @@ createRoot(document.getElementById('root')).render(
     
       </GoogleOAuthProvider>
     </BrowserRouter>
+    </StrictMode>
+   
 )
