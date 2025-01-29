0734d9309092c235f62483faef14ad74303c96ec
kareem <kareemlero@gmail.com>
2025-01-29 10:07:53 +0200

    This is a new commit for what I originally planned to be amended

--- Documents/test-deploy/frontend/vite.config.js
+++ Documents/test-deploy/frontend/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    proxy: {
+      '/api': {
+        target: 'https://vercel-test-five-peach.vercel.app',
+        changeOrigin: true,
+      },
+    },
+    watch:{
+      usePolling: true,
+    }
+  },
   plugins: [react()],
 })
